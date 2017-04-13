6f298b61de78ba0e9e1d038a2f684e63cac002cc
marie_comet <mariecomet666@gmail.com>
2017-04-13 14:50:34 +0200

    Firt commit

---
+++ divi-custom-fonts.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Plugin Name: Divi Custom Fonts
+ * Description: Add custom fonts to Divi theme
+ * Version: 1.0.0
+ * Author: Marie Comet
+ * License: MIT License
+ * Text Domain: afd
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+add_action( 'admin_enqueue_scripts', 'afd_admin_enqueue_scripts' );
+add_action( 'admin_enqueue_scripts', 'afd_enqueue_styles' );
+ 
+function afd_admin_enqueue_scripts() {
+  wp_enqueue_style('fonts-admin', plugin_dir_url( __FILE__ ) .'/fonts/fonts-admin.css');
+}
+add_action( 'wp_enqueue_scripts', 'afd_enqueue_styles' );
+function afd_enqueue_styles() {
+
+    wp_enqueue_style( 'fonts', plugin_dir_url( __FILE__ )  . '/fonts/fonts.css');
+}
+
+function afd_custom_fonts($websafe_fonts) {
+
+	$websafe_fonts['Platform'] = [
+		'styles'    => '400,300,600,700,800',
+		'character_set' => 'latin,cyrillic-ext,greek-ext,greek,vietnamese,latin-ext,cyrillic',
+		'type'      => 'sans-serif',
+	];
+	$websafe_fonts['PublicoTextMono'] = [
+		'styles'    => '400,300,600,700,800',
+		'character_set' => 'latin,cyrillic-ext,greek-ext,greek,vietnamese,latin-ext,cyrillic',
+		'type'      => 'sans-serif',
+	];
+  return $websafe_fonts;
+
+}
+add_filter('et_websafe_fonts', 'afd_custom_fonts');
+
---
+++ readme.txt
@@ -0,0 +1,29 @@
+# Divi Custom Fonts
+
+* Contributors: MarieComet
+* Website link: http://mariecomet.fr/
+* Tags: divi
+* Requires at least: 4.7
+* Tested up to: 4.7.2
+* Stable tag: 1.0
+* License: GPLv2 or later
+* License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Divi Custom Fonts
+
+## Description
+
+Add custom fonts to Divi theme
+
+## Installation
+
+This section describes how to install the plugin and get it working.
+
+1. Upload `divi-custom-fonts` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Refer to plugin description in regards to setting up how the plugins works
+
+## Changelog
+
+### 1.0
+* Initial Release.
